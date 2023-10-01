b2852a173ecc9ab52ea5a2e7954434adf1975d99
Uwimbabazi-Keza <u.keza@alustudent.com>
2023-09-30 18:07:09 -0700

    Poisson distribution

---
+++ math/probability/poisson.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Creates a class Poisson that represents a poisson distribution
+"""
+
+class Poisson:
+	"""
+	Poisson distribution class
+	"""
+	def __init__(self, data=None, lambtha=1.):
+		"""initialize"""
+		 if data is None:
+		     if lambtha <= 0:
+			raise ValueError("lambtha must be a positive value")
+		     else: 
+			self.lambtha = float(lambtha)
+		 else:
+		     if not isinstance(data, list):
+			raise TypeError("data must be a list")
+		     if len(data) < 2:
+			raise ValueError("data must contain multiple values")
+		     self.lambtha = float(sum(data) / len(data))
