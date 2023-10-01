757182fdd66ac2db44fb30472a749f7c9898337e
MwizaKeza <kezauk7@gmail.com>
2023-10-01 05:09:11 +0200

    update

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -16,15 +16,15 @@ class Normal:
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
+            else:
+                self.stddev = float(stddev)
             self.mean = float(mean)
-            self.stddev = float(stddev)
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
             self.mean = float(sum(data) / len(data))
-
-            squared_diff = [(x - self.mean) ** 2 for x in data]
-            self.stddev = (sum(squared_diff) / (len(data) - 1)) ** 0.5
+            self.stddev = (sum([(x - self.mean) ** 2 for x in data])
+                           / len(data)) ** 0.5
+        
