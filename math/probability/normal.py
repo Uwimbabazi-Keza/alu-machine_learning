c5bf0d7623bf9fe5ad6fb7f12118b38ed6b36d2b
MwizaKeza <kezauk7@gmail.com>
2023-10-01 05:31:43 +0200

    update

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -39,3 +39,11 @@ class Normal:
         calculates x-value of a given z-score
         """
         return z * self.stddev + self.mean
+    
+    def pdf(self, x):
+        """
+        calculates the value of the PDF for a given x-value
+        """
+        y = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+        pdf = (1 / (self.stddev * (2 * 3.1415926536) ** 0.5)) * 2.7182818285 ** (y)
+        return pdf
