eb0eafc24c59fbbdfe0586cbed63468dbc347ff5
MwizaKeza <kezauk7@gmail.com>
2023-10-15 03:10:55 +0200

    update

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+a function def np_shape(matrix):
+that calculates the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+    def np_shape(matrix)
+    """
+    return matrix.shape
