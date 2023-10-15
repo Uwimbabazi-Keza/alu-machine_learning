edef2d1ee66ef57c0c955e2140e2422059870cad
MwizaKeza <kezauk7@gmail.com>
2023-10-15 15:16:42 +0200

    update

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -14,9 +14,11 @@ def determinant(matrix):
         raise TypeError("matrix must be a list of lists")
 
     num_rows = len(matrix)
-    if num_rows is 0:
-        return 1
+    if num_rows == 0:
+        raise TypeError("matrix must be a list of lists")
     for row in matrix:
+        if len(row) == 0 and num_rows == 1:
+            return 1
         if len(row) != num_rows:
             raise ValueError("matrix must be a square matrix")
 
@@ -35,3 +37,28 @@ def determinant(matrix):
         d += cofactor if col % 2 == 0 else -cofactor
 
     return d
+
+
+if __name__ == '__main__':
+
+    mat0 = [[]]
+    mat1 = [[5]]
+    mat2 = [[1, 2], [3, 4]]
+    mat3 = [[1, 1], [1, 1]]
+    mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
+    mat5 = []
+    mat6 = [[1, 2, 3], [4, 5, 6]]
+
+    print(determinant(mat0))
+    print(determinant(mat1))
+    print(determinant(mat2))
+    print(determinant(mat3))
+    print(determinant(mat4))
+    try:
+        determinant(mat5)
+    except Exception as e:
+        print(e)
+    try:
+        determinant(mat6)
+    except Exception as e:
+        print(e)
