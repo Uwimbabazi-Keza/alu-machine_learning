0fa6893708dba3e4b8379ebfdd3cfdde8ecb472c
MwizaKeza <kezauk7@gmail.com>
2023-10-15 03:23:14 +0200

    update

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a function def np_matmul(mat1, mat2):
+that performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    def np_matmul(mat1, mat2)
+    """
+    return np.matmul(mat1, mat2)
