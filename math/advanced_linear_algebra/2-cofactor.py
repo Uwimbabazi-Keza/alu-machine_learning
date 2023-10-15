31bfbc6eb09a73f4f66a75e59f92fd190e9dd971
MwizaKeza <kezauk7@gmail.com>
2023-10-15 18:50:43 +0200

    update

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -4,15 +4,16 @@
 def cofactor(matrix): that calculates the cofactor matrix of a matrix
 """
 
-
 minor = __import__('1-minor').minor
+
 
 def cofactor(matrix):
     """
     def cofactor(matrix)
     """
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not \
+            all(isinstance(row, list) for row in matrix):
         raise TypeError('matrix must be a list of lists')
 
     num_rows = len(matrix)
@@ -20,13 +21,11 @@ def cofactor(matrix):
         if len(row) != num_rows:
             raise ValueError('matrix must be a non-empty square matrix')
 
-    
     if num_rows == 1:
         return [[1]]
     elif num_rows == 2:
         return [[matrix[1][1], -matrix[1][0]], [-matrix[0][1], matrix[0][0]]]
-
-    
+  
     else:
         minor_matrix = minor(matrix)
         c_mat = []
