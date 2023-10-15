29ad2e6e3995ee047fa6a05d18ee27df820c1da3
MwizaKeza <kezauk7@gmail.com>
2023-10-15 03:12:57 +0200

    update

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+a function def np_transpose(matrix):
+that transposes matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+    def np_transpose(matrix)
+    """
+    return matrix.transpose()
