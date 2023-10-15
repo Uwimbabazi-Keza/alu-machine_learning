cbcf22d62ba3ec5d6399c843f2f4deceab06794d
MwizaKeza <kezauk7@gmail.com>
2023-10-15 02:54:17 +0200

    update

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+a function def cat_arrays(arr1, arr2): that concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    function def cat_arrays(arr1, arr2)
+    """
+    result = arr1 + arr2
+    return result
