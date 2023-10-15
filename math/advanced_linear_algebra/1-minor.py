ddebc96fb977e4a82bc32e581627b4353f2ed094
MwizaKeza <kezauk7@gmail.com>
2023-10-15 17:34:47 +0200

    update

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -12,23 +12,25 @@ def minor(matrix):
     def minor(matrix)
     """
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a list of lists")
+        raise TypeError('matrix must be a list of lists')
 
     num_rows = len(matrix)
-    if num_rows == 0:
-        raise ValueError("matrix must be a non-empty square matrix")
     for row in matrix:
         if len(row) != num_rows:
-            raise ValueError("matrix must be a non-empty square matrix")
+            raise ValueError('matrix must be a non-empty square matrix')
 
-    minors = []
+    if num_rows == 1:
+        return [[1]]
+    elif num_rows == 2:
+        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
 
-    for i in range(num_rows):
-        minor_row = []
-        for j in range(num_rows):
-            minor_matrix = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
-            minor_row.append(minor_matrix)
+    else:
+        minor_matrix = []
+        for i in range(num_rows):
+            minor_row = []
+            for j in range(num_rows):
+                minors = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
+                minor_row.append(determinant(minors))
+            minor_matrix.append(minor_row)
 
-        minors.append(minor_row)
-
-    return minors
+        return minor_matrix
