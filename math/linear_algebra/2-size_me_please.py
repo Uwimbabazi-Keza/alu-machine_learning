43a2319ce0aa704a36a9f6180ecd1f30a431c4ae
MwizaKeza <kezauk7@gmail.com>
2023-10-15 02:34:18 +0200

    update

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+a function def matrix_shape(matrix): that calculates the shape of a matrix
+"""
+
+
+def matrix_shape(matrix):
+    """
+    a function def matrix_shape(matrix)
+    """
+    shape = []
+    while (type(matrix) is list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
