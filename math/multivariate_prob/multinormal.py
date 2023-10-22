fcb89dc10a73af3129f5b9992a19a876e04be8ce
MwizaKeza <kezauk7@gmail.com>
2023-10-22 23:08:10 +0200

    instance method def pdf(self, x)

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -21,3 +21,14 @@ class MultiNormal:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = (data - self.mean) @ (data - self.mean).T / (n - 1)
+
+    def pdf(self, x):
+        if not isinstance(x, np.ndarray) or x.shape != (self.cov.shape[0], 1):
+            raise ValueError(f"x must be a numpy.ndarray with shape ({self.cov.shape[0]}, 1)")
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        pdf = 1.0 / np.sqrt((2 * np.pi) ** self.cov.shape[0] * det)
+        diff = x - self.mean
+        exponent = -0.5 * diff.T @ inv @ diff
+        pdf *= np.exp(exponent[0, 0])
+        return pdf
