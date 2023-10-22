a30fb8aa3d1509f85965eafb86903725c6413962
MwizaKeza <kezauk7@gmail.com>
2023-10-22 23:20:09 +0200

    update

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -15,19 +15,24 @@ class MultiNormal:
         intialize
         """
         if not isinstance(data, np.ndarray) or data.ndim != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
+            raise TypeError("x must be a numpy.ndarray")
         d, n = data.shape
-        if n < 2:
-            raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = (data - self.mean) @ (data - self.mean).T / (n - 1)
 
     def pdf(self, x):
-        if not isinstance(x, np.ndarray) or x.shape != (self.cov.shape[0], 1):
-            raise ValueError(f"x must be a numpy.ndarray with shape ({self.cov.shape[0]}, 1)")
+        """
+        def pdf
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d, _ = self.cov.shape
+        if x.shape != (d, 1):
+            raise ValueError(f"x must have the shape ({d}, 1)")
+        
         det = np.linalg.det(self.cov)
         inv = np.linalg.inv(self.cov)
-        pdf = 1.0 / np.sqrt((2 * np.pi) ** self.cov.shape[0] * det)
+        pdf = 1.0 / np.sqrt((2 * np.pi) ** d * det)
         diff = x - self.mean
         exponent = -0.5 * diff.T @ inv @ diff
         pdf *= np.exp(exponent[0, 0])
