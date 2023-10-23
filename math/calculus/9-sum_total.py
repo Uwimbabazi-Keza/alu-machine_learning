8d7c233c41a895d1a2929e333c2ff13d6c345813
MwizaKeza <kezauk7@gmail.com>
2023-10-23 21:20:18 +0200

    update

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -10,7 +10,7 @@ def summation_i_squared(n):
     """
     if isinstance(n, int) and n >= 1:
         # Using the formula for the sum of squared values
-        result = (n * (n + 1) * (2 * n + 1)) // 6
+        result = (n * (n + 1) * ((2 * n) + 1)) / 6
         return result
     else:
         return None
