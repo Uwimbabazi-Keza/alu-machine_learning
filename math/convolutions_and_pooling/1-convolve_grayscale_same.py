edc20ddd04d1f45d17c724c49d0716f4369f6ca9
MwizaKeza <kezauk7@gmail.com>
2023-12-13 15:54:49 +0200

    update

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,26 +7,23 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """
-    def convolve_grayscale_same(images, kernel):
+    def convolve_grayscale_valid(images, kernel):
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-
-    pad_h = max(0, (h - kh + kh % 2) // 2)
-    pad_w = max(0, (w - kw + kw % 2) // 2)
-
-    oh, ow = h, w
-
-    convolved_images = np.zeros((m, oh, ow))
-
-    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
-
+    
+    ph = (kh - 1) // 2
+    pw = (kw - 1) // 2
+    
+    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant', constant_values=0)
+    
+    oh = h
+    ow = w
+    
+    result = np.zeros((m, oh, ow))
+    
     for i in range(oh):
         for j in range(ow):
-            image_region = padded_images[:, i:i + kh, j:j + kw]
-
-            convolved_values = np.sum(image_region * kernel, axis=(1, 2))
-
-            convolved_images[:, i, j] = convolved_values
-
-    return convolved_images
+            result[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+    
+    return result
