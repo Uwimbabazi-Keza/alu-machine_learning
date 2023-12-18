51b11e6108d7173c2a8ebb6a3bca1775585c9123
MwizaKeza <kezauk7@gmail.com>
2023-12-18 19:14:24 +0200

    change

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -6,18 +6,21 @@ that calculates the integral of a polynomial
 
 
 def poly_integral(poly, C=0):
+    """def poly_integral(poly, C=0)
     """
-    def poly_integral(poly, C=0)
-    """
-
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly) or not isinstance(C, int):
+    integral = []
+    if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
+    for i in range(len(poly)-1, 0, -1):
+        integral.append(poly[i]/(i+1))
 
-    integral = [C]
+    integral.append(poly[0])
+    integral.append(C)
+    
+    if len(poly) == 1 and poly[0] == 0:
+        integral = [C]
+    for i in range(len(integral)):
+        if integral[i] % 1 == 0:
+            integral[i] = int(integral[i])
 
-    for power, coeff in enumerate(poly):
-        if coeff != 0:
-            new_coeff = coeff / (power + 1)
-            integral.append(new_coeff)
-
-    return integral
+    return integral[::-1]
