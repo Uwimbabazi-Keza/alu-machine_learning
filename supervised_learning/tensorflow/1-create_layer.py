de2b411da0b9aaadbb0fa7d4c0779561c790d4df
MwizaKeza <kezauk7@gmail.com>
2024-01-26 17:14:41 +0200

    Forward propagation

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,7 +7,9 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation):
     """def create_layer(prev, n, activation)"""
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+    mode="FAN_AVG"
+)
 
     layer = tf.layers.Dense(units=n, activation=activation,
                             kernel_initializer=initializer,
---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Function def forward_prop(x, layer_sizes=[], activations=[])
+"""
+
+
+create_layer = __import__('1-create_layer').create_layer
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """creates the forward propagation graph for
+    the neural network
+    """
+
+    prev_layer = x
+
+    for i in range(len(layer_sizes)):
+        n_nodes = layer_sizes[i]
+        activation = activations[i] if i < len(activations) else None
+
+        layer = create_layer(prev_layer, n_nodes, activation)
+
+        prev_layer = layer
+
+    return prev_layer
