11f2f988bb8a3de8e3c8e1e5bba787eada2c5565
MwizaKeza <kezauk7@gmail.com>
2024-01-26 17:22:20 +0200

    corrections

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -5,6 +5,7 @@
 
 create_layer = __import__('1-create_layer').create_layer
 
+
 def forward_prop(x, layer_sizes=[], activations=[]):
     """creates the forward propagation graph for
     the neural network
--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Function def calculate_accuracy(y, y_pred) that calculates the accuracy of a prediction
+"""Function def calculate_accuracy(y, y_pred)
+that calculates the accuracy of a prediction
 """
 
 import tensorflow as tf
@@ -9,7 +10,8 @@ def calculate_accuracy(y, y_pred):
     """calculates the accuracy of a prediction
     """
 
-    correct_predictions = tf.equal(tf.argmax(y, axis=1), tf.argmax(y_pred, axis=1))
+    correct_predictions = tf.equal(tf.argmax(y, axis=1),
+                                   tf.argmax(y_pred, axis=1))
 
     accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
