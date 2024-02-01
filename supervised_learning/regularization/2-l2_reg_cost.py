14ace1dac79bee3e938dc9cca5899089dc57eb73
MwizaKeza <kezauk7@gmail.com>
2024-02-01 23:09:15 +0200

    L2 Regularization Cost

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""function that  calculates the cost of a neural
+network with L2 regularization"""
+
+import tensorflow as tf
+
+def l2_reg_cost(cost, lambtha, weights):
+    """ calculates the cost of a neural
+    network with L2 regularization
+    """
+
+    l2_reg_term = sum(tf.nn.l2_loss(w) for w in weights)
+
+    l2_reg_term *= (lambtha / 2)
+
+    cost_with_reg = cost + l2_reg_term
+
+    return cost_with_reg
