b8216e2772188c312fb9910b9a1007473e2aa146
MwizaKeza <kezauk7@gmail.com>
2024-02-01 22:45:50 +0200

    Learning regularization

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""function that calculates the cost of a neural network 
+with L2 regularization
+"""
+
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """calculates the cost of a neural network
+    with L2 regularization
+    """
+
+    reg_term = 0
+
+    for l in range(1, L + 1):
+        reg_term += np.sum(np.square(weights["W" + str(l)]))
+
+    reg_term *= (lambtha / (2 * m))
+
+    cost_with_reg = cost + reg_term
+
+    return cost_with_reg
