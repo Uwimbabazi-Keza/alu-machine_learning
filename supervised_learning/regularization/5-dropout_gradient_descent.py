670505edaa65f75cfcc32ccad27c8542b4d5dabe
MwizaKeza <kezauk7@gmail.com>
2024-02-02 23:36:00 +0200

    update

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -6,22 +6,21 @@ import numpy as np
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """conducts forward propagation using Dropout"""
-
     m = Y.shape[1]
-
-    for i in range(L, 0, -1):
-        A = cache["A{}".format(i)]
-        dz = A - Y
-
-        db = np.sum(dz, axis=1, keepdims=True) / m
-        dw = np.dot((cache["A{}".format(
-            i - 1)] if i > 1 else cache["A0"]), dz.T) / m
-
-        if i > 1:
-            dz = np.dot(weights["W{}".format(i)].T, dz) * (1 - (
-                cache["A{}".format(i - 1)] if i > 1 else cache["A0"])**2)
-            dz *= cache["D{}".format(i - 1)]
-            dz /= keep_prob
-
-        weights["b{}".format(i)] -= alpha * db
-        weights["W{}".format(i)] -= alpha * dw.T
+    for layer_index in range(L, 0, -1):
+        A = cache['A' + str(layer_index)]
+        A_prev = cache['A' + str(layer_index - 1)]
+        W = weights['W' + str(layer_index)]
+        b = weights['b' + str(layer_index)]
+        if layer_index == L:
+            dZ = A - Y
+        else:
+            dZ = dA * (1 - (A ** 2))
+            dZ = np.multiply(dZ, cache['D' + str(layer_index)])
+            dZ /= keep_prob
+        dW = (1 / m) * np.matmul(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        dA = np.matmul(W.T, dZ)
+        weights['W' + str(layer_index)] = W - (alpha * dW)
+        weights['b' + str(layer_index)] = b - (alpha * db)
+    return weights
--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -7,9 +7,11 @@ import tensorflow as tf
 def dropout_create_layer(prev, n, activation, keep_prob):
     """creates a layer of a neural network using dropout
     """
-    weight_initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    weight_initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
     dropout_layer = tf.layers.Dropout(keep_prob)
     layer_model = tf.layers.Dense(units=n, activation=activation,
-                                  name="layer", kernel_initializer=weight_initializer,
+                                  name="layer", 
+                                  kernel_initializer=weight_initializer,
                                   kernel_regularizer=dropout_layer)
     return layer_model(prev)
