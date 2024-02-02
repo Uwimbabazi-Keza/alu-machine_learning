bc502b3b8f9073fe288be2a2b42c109386a40ea9
MwizaKeza <kezauk7@gmail.com>
2024-02-02 22:10:35 +0200

    update

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -7,28 +7,19 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     """
     conducts forward propagation using Dropout
     """
-    outputs = {}
-    masks = {}
+    outputs = {"A0": X}
 
-    A = X
-    outputs["A0"] = A
+    for layer_index in range(1, L + 1):
+        weight = weights["W{}".format(layer_index)]
+        bias = weights["b{}".format(layer_index)]
+        Z = np.matmul(weight, outputs["A{}".format(layer_index - 1)]) + bias
+        dropout = np.random.binomial(1, keep_prob, size=Z.shape)
 
-    for l in range(1, L):
-        Z = np.dot(weights["W" + str(l)], A) + weights["b" + str(l)]
-        A = np.tanh(Z)
-        
-        mask = (np.random.rand(*A.shape) < keep_prob).astype(int)
-        A = A * mask / keep_prob
+        if layer_index != L:
+            outputs["A{}".format(layer_index)] = np.tanh(Z) * dropout / keep_prob
+            outputs["D{}".format(layer_index)] = dropout
+        else:
+            exp_Z = np.exp(Z)
+            outputs["A{}".format(layer_index)] = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
 
-        outputs["Z" + str(l)] = Z
-        outputs["A" + str(l)] = A
-        masks["D" + str(l)] = mask
-
-    Z_last = np.dot(weights["W" + str(L)], A) + weights["b" + str(L)]
-    exp_Z_last = np.exp(Z_last)
-    A_last = exp_Z_last / np.sum(exp_Z_last, axis=0, keepdims=True)
-
-    outputs["Z" + str(L)] = Z_last
-    outputs["A" + str(L)] = A_last
-
-    return outputs, masks
+    return outputs
