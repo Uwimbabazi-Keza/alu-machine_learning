cf5ff926bfcf20dc2b5af521b9151cc1d5c8ba91
MwizaKeza <kezauk7@gmail.com>
2024-02-02 22:14:20 +0200

    update

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -12,14 +12,17 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     for layer_index in range(1, L + 1):
         weight = weights["W{}".format(layer_index)]
         bias = weights["b{}".format(layer_index)]
-        Z = np.matmul(weight, outputs["A{}".format(layer_index - 1)]) + bias
+        Z = np.matmul(weight, outputs["A{}".format(
+            layer_index - 1)]) + bias
         dropout = np.random.binomial(1, keep_prob, size=Z.shape)
 
         if layer_index != L:
-            outputs["A{}".format(layer_index)] = np.tanh(Z) * dropout / keep_prob
+            outputs["A{}".format(layer_index
+                                 )] = np.tanh(Z) * dropout / keep_prob
             outputs["D{}".format(layer_index)] = dropout
         else:
             exp_Z = np.exp(Z)
-            outputs["A{}".format(layer_index)] = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+            outputs["A{}".format(layer_index)] = exp_Z / np.sum(
+                exp_Z, axis=0, keepdims=True)
 
     return outputs
