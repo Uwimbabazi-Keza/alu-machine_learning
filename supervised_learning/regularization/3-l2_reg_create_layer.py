8c09ce706f4a03bf88c91ad6d41b349a396e6055
MwizaKeza <kezauk7@gmail.com>
2024-02-02 23:50:03 +0200

    update

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -10,17 +10,15 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     that includes L2 regularization
     """
 
-    initializer = tf.keras.initializers.glorot_uniform()
     regularizer = tf.keras.regularizers.l2(lambtha)
-    weights = tf.Variable(initializer(shape=(prev.shape[1], n)), name="kernel", regularizer=regularizer)
+    weight = tf.keras.initializers.VarianceScaling(mode="fan_avg")
     
-    biases = tf.Variable(tf.zeros((1, n)), name="bias")
-
-    z = tf.matmul(prev, weights) + biases
-
-    if activation is not None:
-        output = activation(z)
-    else:
-        output = z
-
-    return output
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        name="layer",
+        kernel_initializer=weight,
+        kernel_regularizer=regularizer
+    )
+    
+    return layer(prev)
