291c4265bfb7b770bd9f876a5c5e9db652cdf1c3
MwizaKeza <kezauk7@gmail.com>
2024-02-09 22:15:51 +0200

    update

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
-    updates the learning rate using 
+    updates the learning rate using
     inverse time decay in numpy
     """
 
