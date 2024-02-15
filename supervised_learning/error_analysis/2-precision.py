65f859308812b8c6de9488f59880ba158a1c0e42
MwizaKeza <kezauk7@gmail.com>
2024-02-15 13:01:20 +0200

    update

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+"""calculates the precision for each class in a confusion matrix:
+
+"""
+
 import numpy as np
 """calculates the precision for each class
-    in a confusion matrix"""
+in a confusion matrix"""
 
 
 def precision(confusion):
