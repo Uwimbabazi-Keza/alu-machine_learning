1b71226c65a0e939b849710c6eb84530bb00d279
MwizaKeza <kezauk7@gmail.com>
2024-02-15 12:51:05 +0200

    update

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -1,9 +1,9 @@
-
 #!/usr/bin/env python3
 """calculates the sensitivity for each
 class in a confusion matrix"""
 
 import numpy as np
+
 
 def sensitivity(confusion):
     """calculates the sensitivity for each
