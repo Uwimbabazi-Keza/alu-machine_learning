a6998c129b724afd1733dd0a8f3819267eb1167e
MwizaKeza <kezauk7@gmail.com>
2024-02-16 14:41:42 +0200

    update

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -8,13 +8,8 @@ precision = __import__('2-precision').precision
 
 def f1_score(confusion):
     """ calculates the F1 score of a confusion matrix"""
-    classes = confusion.shape[0]
-    f1_per_class = np.zeros(classes)
-
-    for i in range(classes):
-        recall = sensitivity(confusion, i)
-        prec = precision(confusion, i)
-
-        f1_per_class[i] = 2 * (prec * recall) / (prec + recall) if (prec + recall) != 0 else 0
-
-    return f1_per_class
+    precise = precision(confusion)
+    sensitive = sensitivity(confusion)
+    F1_score = (2 * precise * sensitive) / (precise + sensitive)
+    
+    return F1_score
