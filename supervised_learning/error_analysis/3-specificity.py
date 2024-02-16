3343ec4b297f0769330d307abb4c2747eacc3978
MwizaKeza <kezauk7@gmail.com>
2024-02-16 15:38:22 +0200

    update

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -8,14 +8,16 @@ import numpy as np
 def specificity(confusion):
     """ calculates the specificity for each
     class in a confusion matrix"""
-    
+
     classes = confusion.shape[0]
     specificity = np.zeros(classes)
 
     for i in range(classes):
-        true_negatives = np.sum(confusion) - np.sum(confusion[i]) - np.sum(confusion[:, i]) + confusion[i, i]
+        true_negatives = np.sum(confusion) - np.sum(
+            confusion[i]) - np.sum(confusion[:, i]) + confusion[i, i]
         false_positives = np.sum(confusion[:, i]) - confusion[i, i]
 
-        specificity[i] = true_negatives / (true_negatives + false_positives) if (true_negatives + false_positives) != 0 else 0
+        specificity[i] = true_negatives / (
+             + false_positives) if (true_negatives + false_positives) != 0 else 0
 
     return specificity
