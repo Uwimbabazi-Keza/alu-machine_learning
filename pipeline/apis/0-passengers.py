33c5bba8ae5548a75f6dd805ad41f3b440d6f94b
MwizaKeza <kezauk7@gmail.com>
2024-03-19 18:27:55 +0200

    Data collection

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""A method that returns the list of ships
+that can hold a given number of passengers"""
+
+import requests
+
+def availableShips(passengerCount):
+    """A method that returns the list of ships
+    that can hold a given number of passengers"""
+
+    url = "https://swapi-api.alx-tools.com/api/starships/"
+    
+    ships = []
+    
+    while url:
+        r = requests.get(url)
+        data = r.json()
+        starships = data.get("results", [])
+        for starship in starships:
+            if starship.get('passengers', '').isdigit():
+                if int(starship['passengers']) >= passengerCount:
+                    ships.append(starship['name'])
+        url = data.get("next")
+    
+    return ships
