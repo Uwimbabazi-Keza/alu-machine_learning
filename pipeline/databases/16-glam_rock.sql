17dc65a882358181a2f0864d4d7144ddbd42d0dc
MwizaKeza <kezauk7@gmail.com>
2024-04-19 02:20:51 +0200

    database

---
+++ pipeline/databases/16-glam_rock.sql
@@ -0,0 +1,8 @@
+-- script that lists bands that have Glam rock as their main style
+SELECT 
+    band_name, IFNULL(split, 2020) - formed AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
