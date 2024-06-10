dd3f3d516c162c1eec28fefca994b0345be6ee34
MwizaKeza <kezauk7@gmail.com>
2024-06-10 13:04:50 +0200

    update

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -6,14 +6,16 @@ import tensorflow as tf
 
 
 class RNNEncoder(tf.keras.layers.Layer):
-    """class RNNEncoder"""
+    """class RNNEncoder that inherits from tensorflow.keras.layers
+    Layer to encode for machine translation"""
 
     def __init__(self, vocab, embedding, units, batch):
         """initialize"""
         super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
-        self.embedding = tf.keras.layers.Embedding(input_dim=vocab, output_dim=embedding)
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
+                                                   output_dim=embedding)
         self.gru = tf.keras.layers.GRU(units,
                                        return_sequences=True,
                                        return_state=True,
