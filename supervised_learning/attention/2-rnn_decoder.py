3c68ab45b13e417f4cb1b4408cc04d7cd68b16a6
MwizaKeza <kezauk7@gmail.com>
2024-06-10 13:55:27 +0200

    update

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -3,36 +3,33 @@
 Layer to decode for machine translation"""
 
 import tensorflow as tf
+from tensorflow.keras.layers import Layer, Embedding, GRU, Dense
 SelfAttention = __import__('1-self_attention').SelfAttention
 
 
-class RNNDecoder(tf.keras.layers.Layer):
+class RNNDecoder(Layer):
     """a class RNNDecoder that inherits from tensorflow.keras.layers.
     Layer to decode for machine translation"""
-
     def __init__(self, vocab, embedding, units, batch):
         """initialize"""
         super(RNNDecoder, self).__init__()
-        self.embedding = tf.keras.layers.Embedding(input_dim=vocab, output_dim=embedding)
-        self.gru = tf.keras.layers.GRU(units,
-                                        return_sequences=True,
-                                        return_state=True,
-                                        recurrent_initializer='glorot_uniform')
-        self.F = tf.keras.layers.Dense(vocab)
+        self.vocab = vocab
+        self.embedding_dim = embedding
+        self.units = units
+        self.batch = batch
+        self.embedding = Embedding(input_dim=vocab, output_dim=embedding)
+        self.gru = GRU(units, return_sequences=True, return_state=True,
+                       recurrent_initializer='glorot_uniform')
+        self.F = Dense(vocab)
+        self.attention = SelfAttention(units)
         
     def call(self, x, s_prev, hidden_states):
         """Decode for machine translation"""
         x = self.embedding(x)
-        
-        s_prev_expanded = tf.expand_dims(s_prev, 1)
-        score = tf.keras.layers.Dense(1)(tf.nn.tanh(s_prev_expanded + hidden_states))
-        weights = tf.nn.softmax(score, axis=1)
-        context_vector = tf.reduce_sum(weights * hidden_states, axis=1)
-        
+        context_vector, attention_weights = self.attention(s_prev, hidden_states)
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        output, state = self.gru(x, initial_state=s_prev)
+        output = tf.reshape(output, (-1, output.shape[2]))
+        y = self.F(output)
         
-        y, s = self.gru(x, initial_state=s_prev)
-        
-        y = self.F(y)
-        
-        return y, s
+        return y, state
