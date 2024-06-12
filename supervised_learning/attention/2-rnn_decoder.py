97525f288440056aec704bdece05e9750179514d
MwizaKeza <kezauk7@gmail.com>
2024-06-12 14:49:04 +0200

    update

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
-"""a class RNNDecoder that inherits from tensorflow.keras.layers.
+"""A class RNNDecoder that inherits from tensorflow.keras.layers.
 Layer to decode for machine translation"""
 
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, Embedding, GRU, Dense
+
 SelfAttention = __import__('1-self_attention').SelfAttention
 
-
-class RNNDecoder(Layer):
-    """a class RNNDecoder that inherits from tensorflow.keras.layers.
+class RNNDecoder(tf.keras.layers.Layer):
+    """A class RNNDecoder that inherits from tensorflow.keras.layers.Layer.
     Layer to decode for machine translation"""
     def __init__(self, vocab, embedding, units, batch):
-        """initialize"""
+        """Initialize the RNNDecoder layer"""
         super(RNNDecoder, self).__init__()
         self.vocab = vocab
         self.embedding_dim = embedding
         self.units = units
         self.batch = batch
-        self.embedding = Embedding(input_dim=vocab, output_dim=embedding)
-        self.gru = GRU(units, return_sequences=True, return_state=True,
-                       recurrent_initializer='glorot_uniform')
-        self.F = Dense(vocab)
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab, output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(units, return_sequences=True, return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+        self.F = tf.keras.layers.Dense(vocab)
         self.attention = SelfAttention(units)
         
     def call(self, x, s_prev, hidden_states):
         """Decode for machine translation"""
-        x = self.embedding(x)
+        x = self.embedding(x) 
         context_vector, attention_weights = self.attention(s_prev, hidden_states)
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
         output, state = self.gru(x, initial_state=s_prev)
