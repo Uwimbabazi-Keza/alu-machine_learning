f1193087d01603204f768849155fcf99c5a29925
MwizaKeza <kezauk7@gmail.com>
2024-06-13 16:41:41 +0200

    update

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -14,5 +14,5 @@ def positional_encoding(max_seq_len, dm):
     div_term = np.exp(np.arange(0, dm, 2) * -(np.log(10000.0) / dm))
     pos_enc[:, 0::2] = np.sin(position * div_term)
     pos_enc[:, 1::2] = np.cos(position * div_term)
-    
+
     return pos_enc
