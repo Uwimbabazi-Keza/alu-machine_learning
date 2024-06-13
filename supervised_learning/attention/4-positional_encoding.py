8a59e43b90045469d1a69c57f57dc0a0c006627a
MwizaKeza <kezauk7@gmail.com>
2024-06-13 16:40:04 +0200

    calculates the positional encoding for a transformer

---
+++ supervised_learning/attention/4-positional_encoding.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""calculates the positional encoding
+for a transformer"""
+
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """calculates the positional encoding
+    for a transformer"""
+
+    pos_enc = np.zeros((max_seq_len, dm))
+    position = np.arange(max_seq_len)[:, np.newaxis]
+    div_term = np.exp(np.arange(0, dm, 2) * -(np.log(10000.0) / dm))
+    pos_enc[:, 0::2] = np.sin(position * div_term)
+    pos_enc[:, 1::2] = np.cos(position * div_term)
+    
+    return pos_enc
