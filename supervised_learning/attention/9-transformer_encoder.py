bcc20da3ca447dc21f38e9140cbf98bb2209f11f
MwizaKeza <kezauk7@gmail.com>
2024-06-14 16:06:56 +0200

    remove numpy import

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -2,7 +2,6 @@
 """create the encoder for a transformer"""
 
 import tensorflow as tf
-import numpy as np
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
@@ -10,9 +9,8 @@ EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 class Encoder(tf.keras.layers.Layer):
     """create the encoder for a transformer"""
     def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
-        """initialize"""
         super(Encoder, self).__init__()
-
+        
         self.N = N
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
