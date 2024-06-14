5b1a9e69d0fbec84378639e40aa58adf656f4755
MwizaKeza <kezauk7@gmail.com>
2024-06-14 15:33:41 +0200

    update

--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -13,7 +13,7 @@ class EncoderBlock(tf.keras.layers.Layer):
     def __init__(self, dm, h, hidden, drop_rate=0.1):
         """initialize"""
         super(EncoderBlock, self).__init__()
-        
+
         self.mha = MultiHeadAttention(dm, h)
         self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
         self.dense_output = tf.keras.layers.Dense(dm)
@@ -21,7 +21,7 @@ class EncoderBlock(tf.keras.layers.Layer):
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
-    
+
     def call(self, x, training, mask=None):
         """
         Perform the forward pass for the encoder block."""
@@ -33,5 +33,5 @@ class EncoderBlock(tf.keras.layers.Layer):
         ffn_output = self.dense_output(ffn_output)
         ffn_output = self.dropout2(ffn_output, training=training)
         out2 = self.layernorm2(out1 + ffn_output)
-        
+
         return out2
