6bfad72e5d256d36ef2b81c9906599568c1754a4
MwizaKeza <kezauk7@gmail.com>
2024-06-15 14:46:08 +0200

    pycodestyle

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 Encoder = __import__('9-transformer_encoder').Encoder
 Decoder = __import__('10-transformer_decoder').Decoder
 
+
 class Transformer(tf.keras.Model):
     """create a transformer network"""
     def __init__(
@@ -50,7 +51,8 @@ class Transformer(tf.keras.Model):
         self.linear = tf.keras.layers.Dense(target_vocab)
 
     def call(
-            self, inputs, target, training, encoder_mask, look_ahead_mask, decoder_mask
+            self, inputs, target, training, encoder_mask,
+            look_ahead_mask, decoder_mask
             ):
         """Perform the forward pass for the transformer."""
         enc_output = self.encoder(inputs, training, encoder_mask)
