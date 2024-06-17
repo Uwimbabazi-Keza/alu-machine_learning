7e382307281abc6fdb31b94fe463e2dfb568d93f
MwizaKeza <kezauk7@gmail.com>
2024-06-17 14:28:40 +0200

    .

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -6,11 +6,11 @@ import tensorflow as tf
 
 SelfAttention = __import__('1-self_attention').SelfAttention
 
+
 class RNNDecoder(tf.keras.layers.Layer):
-    """class RNNDecoder that inherits from tensorflow.keras.layers.Layer.
-    Layer to decode for machine translation"""
+    """Class to decode for machine translation"""
     def __init__(self, vocab, embedding, units, batch):
-        """Initialize the RNNDecoder layer"""
+        """initialize"""
         if type(vocab) is not int:
             raise TypeError(
                 "vocab must be int representing the size of output vocabulary")
@@ -24,23 +24,24 @@ class RNNDecoder(tf.keras.layers.Layer):
             raise TypeError(
                 "batch must be int representing the batch size")
         super(RNNDecoder, self).__init__()
-        self.vocab = vocab
-        self.embedding_dim = embedding
-        self.units = units
-        self.batch = batch
-        self.embedding = tf.keras.layers.Embedding(input_dim=vocab, output_dim=embedding)
-        self.gru = tf.keras.layers.GRU(units, return_sequences=True, return_state=True,
-                                       recurrent_initializer='glorot_uniform')
-        self.F = tf.keras.layers.Dense(vocab)
-        self.attention = SelfAttention(units)
-        
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
+                                                   output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(units=units,
+                                       return_state=True,
+                                       return_sequences=True,
+                                       recurrent_initializer="glorot_uniform")
+        self.F = tf.keras.layers.Dense(units=vocab)
+
     def call(self, x, s_prev, hidden_states):
-        """Decode for machine translation"""
+        """Returns the output word as a one hot vector and
+        the new decoder hidden state"""
+        units = s_prev.get_shape().as_list()[1]
+        attention = SelfAttention(units)
+        context, weights = attention(s_prev, hidden_states)
         x = self.embedding(x)
-        context_vector, attention_weights = self.attention(s_prev, hidden_states)
-        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
-        output, state = self.gru(x, initial_state=s_prev)
-        output = tf.reshape(output, (-1, output.shape[2]))
-        y = self.F(output)
-        
-        return y, state
+        context = tf.expand_dims(context, 1)
+        x = tf.concat([context, x], axis=-1)
+        y, s = self.gru(x)
+        y = tf.reshape(y, (-1, y.shape[2]))
+        y = self.F(y)
+        return y, s
