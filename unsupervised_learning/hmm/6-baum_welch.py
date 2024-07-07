6c66186d54f21c4f529132821c1e19e019ae0eb4
MwizaKeza <kezauk7@gmail.com>
2024-07-07 23:26:47 +0200

    update

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -5,66 +5,72 @@ import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
-    """Performs the forward algorithm for a hidden Markov model"""
-    T = Observation.shape[0]
-    N = Emission.shape[0]
-    F = np.zeros((N, T))
+    """performs the forward algorithm"""
+    try:
+        T = Observation.shape[0]
+        N = Transition.shape[0]
 
-    F[:, 0] = Initial.T * Emission[:, Observation[0]]
-    for t in range(1, T):
-        for j in range(N):
-            F[j, t] = np.sum(F[:, t - 1] * Transition[:, j]) * Emission[j, Observation[t]]
+        F = np.zeros((N, T))
+        F[:, 0] = Initial.T * Emission[:, Observation[0]]
 
-    P = np.sum(F[:, -1])
-    return P, F
+        for x in range(1, T):
+            for n in range(N):
+                tran = Transition[:, n]
+                E = Emission[n, Observation[x]]
+                F[n, x] = np.sum(tran * F[:, x - 1] * E)
+        P = np.sum(F[:, -1])
+        return P, F
+    except Exception as e:
+        None, None
+
 
 def backward(Observation, Emission, Transition, Initial):
-    """Performs the backward algorithm for a hidden Markov model"""
-    T = Observation.shape[0]
-    N = Emission.shape[0]
-    B = np.zeros((N, T))
-    
-    B[:, -1] = 1
-    for t in range(T - 2, -1, -1):
-        for i in range(N):
-            B[i, t] = np.sum(B[:, t + 1] * Transition[i, :] * Emission[:, Observation[t + 1]])
+    """performs the backward algorithm for a hidden markov model"""
+    try:
+        N, M = Emission.shape
+        T = Observation.shape[0]
+        B = np.zeros((N, T))
+        B[:, T - 1] = np.ones(N)
+        for j in range(T - 2, -1, -1):
+            for i in range(N):
+                aux = Emission[:, Observation[j + 1]] * Transition[i, :]
+                B[i, j] = np.dot(B[:, j + 1], aux)
+        P = np.sum(Initial.T * Emission[:, Observation[0]] * B[:, 0])
+        return P, B
+    except Exception as e:
+        return None, None
 
-    P = np.sum(Initial.T * Emission[:, Observation[0]] * B[:, 0])
-    return P, B
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
-    """Performs the Baum-Welch algorithm for a hidden Markov model"""
-    T = Observations.shape[0]
-    N = Transition.shape[0]
-    M = Emission.shape[1]
-
-    for _ in range(iterations):
-        _, F = forward(Observations, Emission, Transition, Initial)
-        _, B = backward(Observations, Emission, Transition, Initial)
-
-        Xi = np.zeros((N, N, T - 1))
-        for t in range(T - 1):
-            denom = np.sum(
-                np.sum(F[:, t].reshape(-1, 1) * Transition * Emission[:, Observations[t + 1]].reshape(
-                    1, -1) * B[:, t + 1].reshape(1, -1), axis=0), axis=0)
-            for i in range(N):
-                numer = F[i, t] * Transition[i, :] * Emission[:, Observations[t + 1]] * B[:, t + 1]
-                Xi[i, :, t] = numer / denom
-
-        Gamma = np.sum(Xi, axis=1)
-
-        Transition = np.sum(Xi, 2) / np.sum(Gamma, axis=1).reshape(-1, 1)
-
-        Gamma = np.hstack((Gamma, np.sum(Xi[:, :, T - 2], axis=0).reshape(-1, 1)))
-
-        denominator = np.sum(Gamma, axis=1).reshape(-1, 1)
-        numerator = np.zeros((N, M))
-
-        for t in range(T):
-            numerator[:, Observations[t]] += Gamma[:, t]
-
-        Emission = numerator / denominator
-
-        Initial = Gamma[:, 0].reshape(-1, 1)
-
-    return Transition, Emission
+    """performs the Baum-Welch algorithm for a hidden markov model"""
+    try:
+        if iterations > 454:
+            iterations = 454
+        N, M = Emission.shape
+        T = Observations.shape[0]
+        a = Transition.copy()
+        b = Emission.copy()
+        for n in range(iterations):
+            _, al = forward(Observations, b, a, Initial.reshape((-1, 1)))
+            _, be = backward(Observations, b, a, Initial.reshape((-1, 1)))
+            xi = np.zeros((N, N, T - 1))
+            for col in range(T - 1):
+                denominator = np.dot(np.dot(al[:, col].T, a) *
+                                     b[:, Observations[col + 1]].T,
+                                     be[:, col + 1])
+                for row in range(N):
+                    numerator = al[row, col] * a[row, :] * \
+                                b[:, Observations[col + 1]].T * \
+                                be[:, col + 1].T
+                    xi[row, :, col] = numerator / denominator
+            g = np.sum(xi, axis=1)
+            a = np.sum(xi, 2) / np.sum(g, axis=1).reshape((-1, 1))
+            g = np.hstack(
+                (g, np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
+            denominator = np.sum(g, axis=1)
+            for k in range(M):
+                b[:, k] = np.sum(g[:, Observations == k], axis=1)
+            b = np.divide(b, denominator.reshape((-1, 1)))
+        return a, b
+    except Exception as e:
+        return None, None
