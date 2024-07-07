ddd1704866521bb7abd6c5cff4371b0ccb5cccf5
MwizaKeza <kezauk7@gmail.com>
2024-07-07 23:13:45 +0200

    whitespace

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -9,33 +9,33 @@ def absorbing(P):
         return False
     if len(P.shape) != 2 or P.shape[0] != P.shape[1]:
         return False
-    
+
     n = P.shape[0]
     
     absorbing_states = np.diag(P) == 1
     
     if not np.any(absorbing_states):
         return False
-    
+
     for i in range(n):
         if not absorbing_states[i]:
             visited = set()
             stack = [i]
-            
+
             while stack:
                 state = stack.pop()
-                
+
                 if state in visited:
                     continue
                 visited.add(state)
-                
+
                 if absorbing_states[state]:
                     break
-                
+
                 for j in range(n):
                     if P[state, j] > 0 and j not in visited:
                         stack.append(j)
             else:
                 return False
-    
+
     return True
